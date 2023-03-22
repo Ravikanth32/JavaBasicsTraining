b67be9c201aa971e8108782168174240982da64d
ravik <rkondepati32@gmail.com>
2023-03-22 23:34:22 +0530

    oops with static

--- src/main/java/org/example/oops/EmailSenderService.java
+++ src/main/java/org/example/oops/EmailSenderService.java
@@ -5,27 +5,31 @@ public class EmailSenderService {
     public void send(String recipients, String body, String subject) throws InterruptedException {
         if (recipients != null) {
             boolean isConnectionEstablished = connectToServer();
-            if (isConnectionEstablished) {
-                String message = body + subject;
-                String deliveryMessage = transferMessage(recipients, message);
-                if (deliveryMessage != null && deliveryMessage.equals("Success"))
-                    System.out.println("Email sent");
-                else
-                    System.out.println("Something wrong email sent failed");
+            String[] emails = recipients.split(",");
+            for (int i = 1; i <= emails.length-1; i++) {
+                String email = emails[i];
+                if (isConnectionEstablished) {
+                    String message = body + subject;
+                    String deliveryMessage = transferMessage(email, message);
+                    if (deliveryMessage != null && deliveryMessage.equals("Success"))
+                        System.out.println("Email sent");
+                    else
+                        System.out.println("Something wrong email sent failed");
+                }
             }
         }
     }
 
     private boolean connectToServer() throws InterruptedException {
         System.out.println("Connecting .......");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         System.out.println("Connected to Server");
         return true;
     }
 
     private String transferMessage(String recipients, String message) throws InterruptedException {
         System.out.println("Message posted " + message + " to" + recipients);
-        Thread.sleep(10000);
+        Thread.sleep(1000);
         return "Success";
     }
 
--- src/main/java/org/example/oops/SenderExample.java
+++ src/main/java/org/example/oops/SenderExample.java
@@ -4,11 +4,14 @@ public class SenderExample {
     public static void main(String[] args) throws InterruptedException {
         boolean isServerAlive = EmailSenderService.ping();
         if (isServerAlive) {
+            //read recitps database
+            // recitps
+            // read fiel get body
+
             EmailSenderService senderService = new EmailSenderService();
-            senderService.send("xyz@gmail.com;abc@gmail.com", "look into this issue", "resolve ticket 123");
+            senderService.send("xyz@gmail.com,abc@gmail.com", "look into this issue", "resolve ticket 123");
         }
         System.out.println("I am out of if within in the main");
-
         EmailSenderService.Example example = new EmailSenderService.Example();
         example.name="something";
 //        example.something();
